33485e72ef9c966d1a9714b91d69dfe54d1a8893
ferredecordt <ferredecordt5002@gmail.com>
2026-05-12 23:07:57 +0200

    changed aanpak2, testing in progress

--- aanpak_2.m
+++ aanpak_2.m
@@ -1,48 +1,70 @@
 clear all; clc;
 
 load("DatasetCV.mat")
-a1 = x;
-a2 = y;
+
+x1 = x;
+x2 = y;
 b = cat;
+
 max_n = 12;
 
 fouten = zeros(max_n,1);
+kruisvalidatiefout = zeros(max_n,1);
 
-for n=1:max_n
-    parfor i = 1:length(a1)
-        xr = x;
+N = length(x1);
+
+for n = 1:max_n
+
+    CV = zeros(N,1);
+
+    for i = 1:N
+
+        % ===== LOOCV split =====
+        xr = x1;
         xr(i) = [];
-        yr = y;
-        yr(i) = [];
-        catr = cat;
-        catr(i) = [];
-    % Model trainen
-    A = build_A(xr, yr, n);
-    beta = GD(A, catr);
 
-    % Testpunt evalueren
-    A_e = build_A(x(i), y(i), n);
-    b_hat = classify(A_e, beta);
-    CV(i) = abs(b(i) - b_hat);
+        yr = x2;
+        yr(i) = [];
+
+        catr = b;
+        catr(i) = [];
+
+        % ===== TRAIN =====
+        A = build_A(xr, yr, n);
+        [Ntr, M] = size(A);
+
+        func = @(beta) (1/Ntr) * sum(log(1 + exp(-catr .* (A * beta))));
+        Dfunc = @(beta) -(1/Ntr) * (A' * (catr ./ (1 + exp(catr .* (A * beta)))));
+
+        x0 = zeros(M,1);
+        beta = GD(func, Dfunc, x0);
+
+        % ===== TEST =====
+        A_e = build_A(x1(i), x2(i), n);
+        b_hat = classify(A_e, beta);
+
+        CV(i) = abs(b(i) - b_hat);
+
     end
 
-    kruisvalidatiefout(n) = 1/length(a1) * sum(CV);
+    kruisvalidatiefout(n) = mean(CV);
     fouten(n) = sum(CV ~= 0);
-    
+
 end
 
-% Resultaten plotten
+% ===== PLOT =====
 figure;
+
 subplot(2,1,1);
-plot(1:max_n, fouten, 'b-o', 'LineWidth', 2, 'MarkerFaceColor', 'b');
+plot(1:max_n, fouten, 'b-o', 'LineWidth', 2);
 xlabel('n'); ylabel('Aantal fouten');
-title('LOOCV: testfouten in functie van n');
+title('LOOCV: testfouten');
 grid on;
 
 subplot(2,1,2);
-plot(1:max_n, kruisvalidatiefout, 'r-o', 'LineWidth', 2, 'MarkerFaceColor', 'r');
-xlabel('n'); ylabel('CV-waarde');
-title('LOOCV: kruisvalidatiefout in functie van n');
+plot(1:max_n, kruisvalidatiefout, 'r-o', 'LineWidth', 2);
+xlabel('n'); ylabel('CV fout');
+title('LOOCV: kruisvalidatiefout');
 grid on;
 
 disp(fouten)
